4ca45931f30c152e66d0d2443d5b6700d2da0f61
Paulo Bruno <paulo.bf111@gmail.com>
2023-07-30 20:12:29 -0300

    Comments added

---
+++ components/Comment.js
@@ -0,0 +1,100 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { useContext } from 'react'
+import IconInfo from '@/components/IconInfo'
+import { FiEdit } from 'react-icons/fi'
+import { AiOutlineDelete } from 'react-icons/ai'
+import styles from '@/styles/noticiaById.module.css'
+import { useState } from 'react'
+import AuthContext from '@/context/AuthContext'
+import useAxios from '@/services/useAxios'
+const Comment = ({ comment, id, commentUdpated, handleDelete }) => {
+    const [isEditing, setIsEditing] = useState([]);
+    const [commentEditData, setCommentEditData] = useState(comment.description);
+    const [commentEditError, setCommentEditError] = useState();
+    const [commentEditOk, setCommentEditOk] = useState();
+    const { user } = useContext(AuthContext);
+    const api = useAxios();
+
+    const handleInputEditComment = (event) => {
+        setCommentEditData(event.target.value);
+        setCommentEditError('');
+    }
+
+    const handleEdit = (id) => {
+        const updateIsEditing = [...isEditing];
+        updateIsEditing[id] = true;
+        setIsEditing(updateIsEditing);
+    }
+    const handleCancel = (id) => {
+        const updateIsEditing = [...isEditing];
+        updateIsEditing[id] = false;
+        setIsEditing(updateIsEditing);
+        setCommentEditData(comment.description);
+        setCommentEditError('');
+    }
+
+    const submitUpdateComment = async () => {
+        if(!commentEditData){
+            setCommentEditError("Campo obrigatório!")
+            return;
+        }
+
+        const data = {
+            "description": commentEditData
+        }
+
+        try{
+            const response = await api.patch(`http://localhost:8000/api/update_comment/${comment.id}`, data, {
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            commentUdpated(response.data.message); 
+            const updateIsEditing = [...isEditing];
+            updateIsEditing[id] = false;
+            setIsEditing(updateIsEditing);
+        }catch(error){
+            if(error.response){
+                setCommentEditError(error.response.data?.error);
+            }
+        }
+    }
+
+    return (
+        <div key={comment.id} id={`comment-${id}`} className={styles.grid_comments}>
+            <div className={styles.flex}>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <img src="/assets/images/profile_photo.webp" width={40} height={40} alt="" />
+                    <div>
+                        <p style={{ fontWeight: 'bold' }}>{comment.user.nome} {comment.user.sobrenome}</p>
+                        <p>{comment.published_at === comment.updated_at ?
+                            `Publicado • ${new Date(comment.published_at).toLocaleString()}` :
+                            `Atualizado • ${new Date(comment.updated_at).toLocaleString()}`}</p>
+                    </div>
+                </div>
+                {!isEditing[id] ? user?.user_id === comment?.user.id ? <div style={{ display: 'flex', gap: '5px' }}>
+                    <FiEdit className={styles.icon} onClick={() => handleEdit(id)} />
+                    <AiOutlineDelete className={styles.icon} onClick={()=>handleDelete(comment.id)}/>
+                </div>
+                    : ''
+                    : ''
+                }
+            </div>
+            {!isEditing[id] ? <div style={{ fontSize: '14px', textAlign: 'justify', color: '#333' }}>{comment.description}</div> :
+                <>
+                    <textarea value={commentEditData ? commentEditData : ''} 
+                    style={commentEditError ? { border: '1px solid red' } : {border: '1px solid black'}}
+                    type="text"  name="comentario" onChange={handleInputEditComment} placeholder='Digite um comentário' />
+                    {commentEditError ? <p style={{fontSize: 12, color:'red'}}>{commentEditError}</p> : ''}
+                    <div style={{ display: 'flex', gap: '5px', alignItems: 'flex-start' }} className={styles.edit_comment}>
+                        <button href="" onClick={() => handleCancel(id)} >Cancelar</button>
+                        <button href=""  onClick={submitUpdateComment} disabled={commentEditData ? false : true}>Atualizar</button>
+                    </div>
+                </>
+            }
+
+        </div>
+    )
+}
+
+export default Comment
--- pages/noticias/[title]/[postId]/index.js
+++ pages/noticias/[title]/[postId]/index.js
@@ -9,18 +9,23 @@ import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { FaRegComments } from 'react-icons/fa'
 import { GrClose } from 'react-icons/gr'
-import IconInfo from '@/components/IconInfo'
-import { FiEdit } from 'react-icons/fi'
-import { AiOutlineDelete } from 'react-icons/ai'
 import AuthContext from '@/context/AuthContext'
 import useAxios from '@/services/useAxios'
+import Comment from '@/components/Comment'
 
 const NoticiaById = () => {
   const [post, setPost] = useState();
   const [userData, setUserData] = useState();
+  const [comments, setComments] = useState();
+  const [commentData, setCommentData] = useState();
+  const [commentError, setCommentError] = useState();
+  const [commentOk, setCommentOk] = useState();
+  const [idCommentDelete, setIdCommentDelete] = useState();
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
   const { postId } = router.query;
   const [isToggle, setIsToggle] = useState(false);
+  const [isToggleDelete, setIsToggleDelete] = useState(false);
   const { user } = useContext(AuthContext);
   const api = useAxios();
 
@@ -28,17 +33,31 @@ const NoticiaById = () => {
     fetchData();
     if (user?.email)
       fetchUser();
+    fetchComments();
   }, [])
 
   const handleComments = () => {
     setIsToggle(!isToggle);
+    setIsToggleDelete(false);
+  }
+
+  const fetchComments = async () => {
+    try{
+      const response = await axios.get(`http://localhost:8000/api/list_comments_by_post/${postId}`);
+      setComments(response.data);
+    }catch(error){
+      console.log(error.response.data?.error)
+    }
   }
 
   const fetchData = async () => {
     try {
+      setIsLoading(true);
       const response = await axios.get(`http://localhost:8000/api/list_post_by_id/?id=${postId}`);
+      setIsLoading(false);
       setPost(response.data);
     } catch (error) {
+      setIsLoading(false);
       console.log(error);
     }
   }
@@ -52,13 +71,85 @@ const NoticiaById = () => {
     }
   }
 
+  const handleInputComment = (event) => {
+    setCommentData(event.target.value);
+    setCommentError('');
+  }
+
+  const submitComment = async () => {
+    if (!commentData) {
+      setCommentError("Campo obrigatório")
+      return;
+    }
+
+    const data = {
+      "user": user?.user_id,
+      "post": post?.id,
+      "description": commentData
+    }
+    try{
+      const response = await api.post(`http://localhost:8000/api/insert_comment/`,data, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      setCommentOk(response.data.message);
+      fetchComments();
+      setCommentData('');
+      const timeout = setTimeout(()=> {
+        setCommentOk('');
+      },2500)
+      return ()=> clearTimeout(timeout);
+    }catch(error){
+      if(error.response){
+        setCommentError(error.response.data?.error);
+      }
+    }
+
+  }
+
+  const deleteComment = async () =>{
+    try{
+      await api.delete(`http://localhost:8000/api/delete_comment/${idCommentDelete}`);
+      setCommentOk("Comentário excluído com sucesso!");
+      const timeout = setTimeout(()=> {
+        setCommentOk('');
+      },2500)
+      fetchComments();
+      setIsToggleDelete(false);
+      setIdCommentDelete('');
+      return ()=> clearTimeout(timeout);
+    }catch(error){
+      console.log(error.response.data?.error);
+    }
+  }
+
+  const handleShowMessage = (value) => {
+    setCommentOk(value);
+    const timeout = setTimeout(()=> {
+      setCommentOk('');
+    },2500)
+    fetchComments();
+    return ()=> clearTimeout(timeout);
+  }
+
+  const handleDelete = async (value) => {
+    setIsToggleDelete(true);
+    setIdCommentDelete(value);
+  }
+  const handleDeleteClose = () => {
+    setIsToggleDelete(false);
+  }
+
 
   return (
     <>
       <Navbar />
+      
       <div style={{ minHeight: '120vh' }}>
-
-        <div className={styles.noticia_container}>
+      
+        {
+          !isLoading ? <div className={styles.noticia_container}>
           <div className={styles.noticia_title}>
             <h1>{post?.title}</h1>
           </div>
@@ -75,13 +166,18 @@ const NoticiaById = () => {
           <div className={styles.noticia_comments}>
             <div onClick={handleComments}>
               <FaRegComments style={{ fontSize: 20 }} />
-              <p>0 Comentários</p>
+              <p>{comments?.length != 1 ? comments?.length + " Comentários" : comments?.length + " Comentário"} </p>
             </div>
           </div>
           <div className={styles.noticia_content}>
             <div dangerouslySetInnerHTML={{ __html: '<div style="line-height:1.2;word-wrap:break-word">' + post?.content + '</div>' }} />
           </div>
+        </div> : 
+        <div style={{display:'flex',alignItems:'center',height:'80vh', justifyContent:'center'}}>
+          <img src="/assets/images/loading.svg"  width={50} height={50} alt="" />
         </div>
+        }
+        
       </div>
       <div className={styles.noticia_sidebar_comments}>
         <div onClick={handleComments} className={`${styles.overlay} ${isToggle ? styles.overlay_toggle : ''}`}></div>
@@ -92,43 +188,56 @@ const NoticiaById = () => {
               <GrClose onClick={handleComments} style={{ fontSize: '25px' }} />
             </div>
             <br />
+            {commentOk ? <div className={styles.messageOk}>
+              {commentOk}
+            </div> : ''}
+            {commentError ? <div className={styles.commentError}>
+              {commentError}
+            </div> : ''}
             <div>
               {user?.email ?
                 (<div>
                   <div className={styles.info_user}>
                     <div className={styles.info_user_name}>
                       <img src="/assets/images/profile_photo.webp" width={40} height={40} alt="" />
-                      <p style={{fontWeight: 'bold/;'}}>{userData?.nome} {userData?.sobrenome}</p>
+                      <p style={{ fontWeight: 'bold' }}>{userData?.nome} {userData?.sobrenome}</p>
                     </div>
                     <div>
-                      <button className={styles.info_btn} disabled>Publicar</button>
+                      <button className={styles.info_btn} disabled={commentData ? false : true} onClick={submitComment}>Publicar</button>
                     </div>
                   </div>
-                  <textarea type="text" style={{ width: '100%', marginTop: '10px' }} placeholder='Digite um comentário' />
+                  <textarea value={commentData ? commentData : ''} type="text" name="comentario" onChange={handleInputComment}
+                    style={commentError ? { border: '1px solid red', width: '100%', marginTop: '10px' }
+                      : { border: '1px solid black', width: '100%', marginTop: '10px' }} placeholder='Digite um comentário' />
+                  {commentError ? <p style={{ fontSize: 12, color: 'red' }}>{commentError}</p> : ''}
                 </div>) :
-                <p style={{ fontSize: 12, textAlign:'center' }}>Você precisa estar logado para comentar!</p>
+                <p style={{ fontSize: 12, textAlign: 'center' }}>Você precisa estar logado para comentar!</p>
               }
             </div>
             <br />
-
-            <div className={styles.grid_comments}>
-              <div className={styles.flex}>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <img src="/assets/images/profile_photo.webp" width={40} height={40} alt="" />
-                  <div>
-                    <p style={{ fontWeight: 'bold' }}>Paulo Bruno</p>
-                    <p>Paulo Bruno</p>
-                  </div>
+            {
+              comments?.length > 0 ?
+              comments.map((comment,index) => {
+                  return <Comment id={index} key={comment.id} handleDelete={handleDelete} commentUdpated={handleShowMessage} comment={comment}/>
+                })
+                : <div className={styles.grid_comments}>
+                  <p style={{ fontSize: 12, textAlign: 'center', fontWeight: 'lighter' }}>Sem comentários!</p>
                 </div>
-                <div style={{ display: 'flex', gap: '5px' }}>
-                  <IconInfo icon={<FiEdit className={styles.icon} />} iconText="Editar" />
-                  <IconInfo icon={<AiOutlineDelete className={styles.icon} onClick={() => handleOpenModal(item.id)} />} iconText="Apagar" />
-                </div>
-              </div>
-              <div style={{ fontSize: '14px', textAlign: 'justify', color: '#333' }}>Reprehenderit non ipsum non consequat elit. Pariatur irure laborum Lorem ea exercitation tempor. Fugiat commodo ipsum proident eu ullamco sint deserunt labore nostrud proident et laborum Lorem non.</div>
-            </div>
+            }
           </div>
         </div>
+      </div>
+      <div className={`${styles.delete_comment_container} ${isToggleDelete ? styles.delete_comment_container_toggle: ''}`}>
+            <div className={styles.delete_card}> 
+              <h2>Excluir Comentário</h2>
+              <p>Tem certeza que você quer excluir esse comentário?</p>
+              <div>
+                <a onClick={()=>setIsToggleDelete(false)}>Cancelar</a>
+                <a onClick={deleteComment}>Apagar Comentário</a>
+              </div>
+            </div>
+            <div className={styles.delete_overlay} onClick={handleDeleteClose}></div>
+
       </div>
       <Footer />
     </>
