175493c716c2c888a1d0a85056467f9697227fb1
Paulo Bruno <paulo.bf111@gmail.com>
2023-08-02 15:38:04 -0300

    Sidebar updated

--- components/SideBar.js
+++ components/SideBar.js
@@ -2,11 +2,11 @@
 import React, { useContext } from 'react'
 import styles from '@/styles/sideBar.module.css';
 import AuthContext from '@/context/AuthContext';
-import {CiHome,CiViewList} from 'react-icons/ci'
+import { CiHome, CiViewList, CiUser, CiChat1, CiGrid42 } from 'react-icons/ci'
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-const SideBar = ({isOpen, isAdmin, handleCloseSideBar}) => {
-    const {user} = useContext(AuthContext);
+const SideBar = ({ isOpen, isAdmin, handleCloseSideBar }) => {
+    const { user } = useContext(AuthContext);
     const router = useRouter();
 
     return (
@@ -16,18 +16,42 @@ const SideBar = ({isOpen, isAdmin, handleCloseSideBar}) => {
                     <img src="/assets/images/logo2.jpg" width="240" height="86" alt="" />
                 </div>
                 <div className={styles.options}>
-                    <Link  href={'/'}>
+                    <Link href={'/'}>
                         <div className={`${router.pathname === '/' ? styles.selected : ''}`}>
-                            <CiHome style={{fontSize: 40}}/>
+                            <CiHome style={{ fontSize: 40 }} />
                             <p>Página Inicial</p>
                         </div>
                     </Link>
+                    {user?.email ?
+                        <>
+                            <Link href={'/user/perfil'}>
+                                <div className={`${router.pathname === '/user/perfil' ? styles.selected : ''}`}>
+                                    <CiUser style={{ fontSize: 40 }} />
+                                    <p>Perfil</p>
+                                </div>
+                            </Link>
+                            <Link href={'/user/comments'}>
+                                <div className={`${router.pathname === '/user/comments' ? styles.selected : ''}`}>
+                                    <CiChat1 style={{ fontSize: 40 }} />
+                                    <p>Seus Comentários</p>
+                                </div>
+                            </Link>
+                        </> : ''}
+                    {user?.isAdmin ?
+
+                        <Link href={'/dashboard'}>
+                            <div className={`${router.pathname === '/dashboard' ? styles.selected : ''}`}>
+                                <CiGrid42 style={{ fontSize: 40 }} />
+                                <p>Dashboard</p>
+                            </div>
+                        </Link> : ''}
                     <Link href={'/noticias'}>
                         <div className={`${router.pathname === '/noticias' ? styles.selected : ''}`}>
-                            <CiViewList style={{fontSize: 40}}/>
+                            <CiViewList style={{ fontSize: 40 }} />
                             <p>Notícias</p>
                         </div>
                     </Link>
+
                 </div>
             </div>
             <div className={`${styles.overlay} ${isOpen ? styles.overlay_toggle : ''}`} onClick={handleCloseSideBar}></div>
